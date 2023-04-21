01fc91978e7c6df7b64704e393bbe1d8cc876fa7
Brizhak Ivan <portable3999@gmail.com>
2023-04-21 23:10:53 +0300

    homework8.2

--- src/main/java/ua/hillel/brizhak/homeworks/homework8/Homework8.java
+++ src/main/java/ua/hillel/brizhak/homeworks/homework8/Homework8.java
@@ -4,14 +4,15 @@ public class Homework8 {
         public static void main(String[] args) {
 
             int j = 1;
-            for ( int i = 1 ; j <= 100; i++ , j++) {
+            int n = 100;
+            for ( int i = 1 ; j <= n; i++ , j++) {
                 int a = i / 10;
                 int b = i % 10;
                 int c = i / 10 % 10;
                 if (i == 4 || i == 9 || a == 4 || a == 9 || b == 9 || b == 4 || c == 9 || c == 4) {
                     j = j - 1;
                     continue;
-                } else if (j == 101) {
+                } else if (j == n + 1) {
                     break;
                 }
                 System.out.print(" " + i);
