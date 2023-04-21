98f3f2f4eb0cb104e300345a7fe19c9261dae47f
Brizhak Ivan <portable3999@gmail.com>
2023-04-21 19:44:52 +0300

    homework8.1

--- src/main/java/ua/hillel/brizhak/homeworks/homework7/Homework7.java
+++ src/main/java/ua/hillel/brizhak/homeworks/homework7/Homework7.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Homework7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+
         System.out.println("Greetings! Try to guess the number from 0 to 10. You have 3 attempts");
         int j = 11;
         int random = (int) (Math.random() * j);
@@ -13,21 +15,22 @@ public class Homework7 {
         int a = 0;
         for (int i = 0; i < n; i++, a++ , b--) {
             int userValue = scanner.nextInt();
-            if (userValue == random) {
-                System.out.println("Graz!!! That number was " + random + "!!!");
-                break;
-            } else if (userValue > 10) {
-                b = b + 1;
-                i = i - 1;
-                a = a - 1;
-                System.out.println("Try to guess the number from 0 to 10");
-            } else if (userValue != random && i == n - 1) {
-                System.out.println("Wrong!! You lost the game. Try again later)");
-            } else if (userValue != random && i == a) {
-                a = a++;
-                System.out.println("Wrong!! Try again. " + b + " tries left");
+                if (userValue == random) {
+                    System.out.println("Graz!!! That number was " + random + "!!!");
+                    break;
+                } else if (userValue > 10) {
+                    b = b + 1;
+                    i = i - 1;
+                    a = a - 1;
+                    System.out.println("Try to guess the number from 0 to 10");
+                } else if (userValue != random && i == n - 1) {
+                    System.out.println("Wrong!! You lost the game. Try again later)");
+                } else if (userValue != random && i == a) {
+                    a = a++;
+                    System.out.println("Wrong!! Try again. " + b + " tries left");
+                }
+//            System.out.println(random);
             }
         }
     }
-}
 
