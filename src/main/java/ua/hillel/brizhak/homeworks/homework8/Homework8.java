89a8337bc705a5314d6e7fa1456dac2554ed8857
Brizhak Ivan <portable3999@gmail.com>
2023-04-22 20:13:22 +0300

    homework8.3

--- src/main/java/ua/hillel/brizhak/homeworks/homework8/Homework8.java
+++ src/main/java/ua/hillel/brizhak/homeworks/homework8/Homework8.java
@@ -9,7 +9,8 @@ public class Homework8 {
                 int a = i / 10;
                 int b = i % 10;
                 int c = i / 10 % 10;
-                if (i == 4 || i == 9 || a == 4 || a == 9 || b == 9 || b == 4 || c == 9 || c == 4) {
+                int d = i / 100;
+                if (i == 4 || i == 9 || a == 4 || a == 9 || b == 9 || b == 4 || c == 9 || c == 4 || d == 4 || d == 9) {
                     j = j - 1;
                     continue;
                 } else if (j == n + 1) {
