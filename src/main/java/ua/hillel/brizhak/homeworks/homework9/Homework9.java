89e654cca435e9602248129cc7786c0073692aec
Brizhak Ivan <portable3999@gmail.com>
2023-04-26 03:49:31 +0300

    homework9

--- src/main/java/ua/hillel/brizhak/homeworks/homework9/Homework9.java
+++ src/main/java/ua/hillel/brizhak/homeworks/homework9/Homework9.java
@@ -39,6 +39,33 @@ public class Homework9 {
                 team2Age = team2Age + team2[i];
             }
         }
+
+
+        /*
+        for (int i = 0; i < team1.length; i++) {
+            team1[i] = (int) ((Math.random() * (max - min + 1)) + min);
+            team2[i] = (int) ((Math.random() * (max - min + 1)) + min);
+            if (team1.length - 1 == i) {
+                team1Age = team1Age + team1[i];
+                team2Age = team2Age + team2[i];
+            } else {
+                team1Age = team1Age + team1[i];
+                team2Age = team2Age + team2[i];
+            }
+        }
+        String str1 = Arrays.toString(team1);
+        String str2 = Arrays.toString(team2);
+        System.out.println("Team 1 = " + str1);
+        System.out.println("Team 1 age = " + team1Age);
+        System.out.println("Avarage age 1 team = " + (double) team1Age / team1.length);
+        System.out.println("Team 2 = " + str2);
+        System.out.println("Team 2 age = " + team2Age);
+        System.out.println("Avarage age 2 team = " + (double) team2Age / team2.length);
+        */ // это второй вариант но тут в перечеслении возраста есть скобки
+
+
+
+
         if ((double) team2Age / team2.length > (double) team1Age / team2.length) {
             System.out.println("Team 2 is older than team 1");
         } else if ((double) team2Age / team2.length < (double) team1Age / team2.length) {
