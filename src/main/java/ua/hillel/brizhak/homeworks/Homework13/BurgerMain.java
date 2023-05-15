31e493078016c2a2e25a207ec49808921d747894
Brizhak Ivan <portable3999@gmail.com>
2023-05-15 16:18:05 +0300

    12345

--- src/main/java/ua/hillel/brizhak/homeworks/Homework13/BurgerMain.java
+++ src/main/java/ua/hillel/brizhak/homeworks/Homework13/BurgerMain.java
@@ -3,6 +3,7 @@ package ua.hillel.brizhak.homeworks.Homework13;
 public class BurgerMain {
     public static void main(String[] args) {
         Burger classic = new Burger("булочка", "м'ясо", "сир", "зелень", "майонез", false);
+//        Burger classic1 = new Burger("булочка", "м'ясо", "сир", "зелень", "майонез");
         Burger classicWithoutSauce = new Burger("булочка", "м'ясо", "сир", "зелень");
         Burger doubleMeat = new Burger("булочка", "м'ясо", "сир", "зелень", "майонез", true);
     }
