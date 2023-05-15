fafe6d481a132943ca0283c192a390409076d8cd
Brizhak Ivan <portable3999@gmail.com>
2023-05-15 16:17:16 +0300

    12345

--- src/main/java/ua/hillel/brizhak/homeworks/Homework13/Burger.java
+++ src/main/java/ua/hillel/brizhak/homeworks/Homework13/Burger.java
@@ -7,15 +7,7 @@ public class Burger {
     String cheese;
     String greens;
     String sauce;
-//    boolean bun;
-//    boolean meat;
-//    boolean cheese;
-//    boolean greens;
-//    boolean sauce;
     boolean doubleMeat;
-
-
-
 
     public Burger(String bun, String meat, String cheese, String greens, String sauce) {
         this.bun = bun;
@@ -46,38 +38,19 @@ public class Burger {
         this.greens = greens;
         this.sauce = sauce;
         this.doubleMeat = doubleMeat;
-        System.out.println("Classic burger: "
-                + this.bun + " подвійне "
-                + this.meat + " " + this.cheese
-                + " " + this.greens + " "
-                + this.sauce);
+        if (doubleMeat == true) {
+            System.out.println("Classic burger: "
+                    + this.bun + " подвійне "
+                    + this.meat + " " + this.cheese
+                    + " " + this.greens + " "
+                    + this.sauce);
+        } else if (doubleMeat == false) {
+                System.out.println("Classic burger: "
+                        + this.bun + " "
+                        + this.meat + " " + this.cheese
+                        + " " + this.greens + " "
+                        + this.sauce);
+        }
+    }
     }
 
-
-//    public Burger(boolean bun, boolean meat, boolean cheese, boolean greens, boolean sauce) {
-//        this.bun = bun;
-//        this.meat = meat;
-//        this.cheese = cheese;
-//        this.greens = greens;
-//        this.sauce = sauce;
-//        System.out.println("Classic burger: булочка, м'ясо, сир, зелень, майонез");
-//    }
-//
-//    public Burger(boolean bun, boolean meat, boolean cheese, boolean greens) {
-//        this.bun = bun;
-//        this.meat = meat;
-//        this.cheese = cheese;
-//        this.greens = greens;
-//        System.out.println("Classic burger: булочка, м'ясо, сир, зелень");
-//    }
-//
-//    public Burger(boolean bun, boolean meat, boolean cheese, boolean greens, boolean sauce, boolean doubleMeat) {
-//        this.bun = bun;
-//        this.meat = meat;
-//        this.cheese = cheese;
-//        this.greens = greens;
-//        this.sauce = sauce;
-//        this.doubleMeat = doubleMeat;
-//        System.out.println("Classic burger: булочка, подвійне м'ясо, сир, зелень, майонез");
-//    }
-}
--- src/main/java/ua/hillel/brizhak/homeworks/Homework13/BurgerMain.java
+++ src/main/java/ua/hillel/brizhak/homeworks/Homework13/BurgerMain.java
@@ -2,13 +2,8 @@ package ua.hillel.brizhak.homeworks.Homework13;
 
 public class BurgerMain {
     public static void main(String[] args) {
-        Burger classic = new Burger("булочка", "мясо", "сир", "зелень", "майонез");
-        Burger classicWithoutSauce = new Burger("булочка", "мясо", "сир", "зелень");
-        Burger doubleMeat = new Burger("булочка", "мясо", "сир", "зелень", "майонез", true);
-
-
-//        Burger classic = new Burger(true, true, true, true, true);
-//        Burger classicWithoutSauce = new Burger(true, true, true, true);
-//        Burger doubleMeat = new Burger(true, true, true, true, true, true);
+        Burger classic = new Burger("булочка", "м'ясо", "сир", "зелень", "майонез", false);
+        Burger classicWithoutSauce = new Burger("булочка", "м'ясо", "сир", "зелень");
+        Burger doubleMeat = new Burger("булочка", "м'ясо", "сир", "зелень", "майонез", true);
     }
 }
