0c2de03ef3f4e491f46e573f7090241ff67e0da2
Brizhak Ivan <portable3999@gmail.com>
2023-05-15 11:26:02 +0300

    1234

--- src/main/java/ua/hillel/brizhak/homeworks/Homework12/Person.java
+++ src/main/java/ua/hillel/brizhak/homeworks/Homework12/Person.java
@@ -2,21 +2,22 @@ package ua.hillel.brizhak.homeworks.Homework12;
 
 public class Person {
     public static void main(String[] args) {
-        personInfo("Will",
+
+        System.out.println(personInfo("Will",
                 " Smith",
                 "New York",
-                2936729462846L);
-        personInfo("Jackie",
+                2936729462846L));
+        System.out.println(personInfo("Jackie",
                 " Chan",
                 "Shanghai",
-                12312412412L);
-        personInfo("Sherlock",
+                12312412412L));
+        System.out.println(personInfo("Sherlock",
                 " Holmes",
                 "London",
-                37742123513L);
+                37742123513L));
     }
-    static void personInfo(String name, String surname, String city, long phoneNumber) {
-        System.out.println("Зателефонувати громадянинові " + name + surname + " з міста " + city + " можна за номером " + phoneNumber);
+    static String personInfo(String name, String surname, String city, long phoneNumber) {
+        return "Зателефонувати громадянинові " + name + surname + " з міста " + city + " можна за номером " + phoneNumber;
     }
 
 }
